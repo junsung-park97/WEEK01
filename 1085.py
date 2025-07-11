296f888e1e72b3b9132df494eccd86592227605f
junSung <not971006@gmail.com>
2025-07-11 15:57:43 +0900

    백준 2739 문제풀이

--- 1085.py
+++ 1085.py
@@ -3,11 +3,17 @@
 
 x,y,w,h = map(int,input().split())
 
+# 각 변까지의 거리
 to_left = x
 to_button = y
 to_right = w - x
 to_top = h - y
 
+# 변까지의 거리를 구한 각각의 값을 리스트에 담는다
 distances = [to_left, to_button, to_right, to_top]
 
+# 그 값중에 최소값을 출력한다
 print(min(distances))
+
+# (0, 0)부터 (w, h)까지의 직사각형안에 한수가(x, y)에 존재하는데 직사각형의 경계까지의 최소거리를 구하는 문제
+# 한수의 좌표로부터 각 변의 거리를 구한 후 그 값중에 최소값을 구한다
--- 3.py
+++ 3.py
@@ -5,7 +5,6 @@ B = int(input())
 temp1 = int(B % 10) * A
 temp10 = int((B/10)%10) * A
 temp100 = int((B/100)%10) * A
-
 print(temp1)
 print(temp10)
 print(temp100)
