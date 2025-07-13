96b6cad9ea9a6e13d2f4ff9dff9deef1bf3664b8
junSung <not971006@gmail.com>
2025-07-14 00:05:14 +0900

    백준 10989 카운트정렬
    이해가 안돼 복습할 것.

--- 10989.py
+++ 10989.py
@@ -1,19 +1,39 @@
+
+# 퀵 정렬
+# import sys
+# sys.setrecursionlimit(10**7)
+
+# #퀵정렬정의
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[0]
+#     left = [x for x in arr[:1] if x <= pivot]
+#     right = [x for x in arr[:1] if x > pivot]
+
+#     return quick_sort(left) + [pivot] + quick_sort(right)
+
+# N = int(sys.stdin.readline())
+# nums = [int(sys.stdin.readline()) for _ in range(N)]
+
+# sort_nums = quick_sort(nums)
+# for num in sort_nums:
+#     print(num)
+
+#카운트 정렬
 import sys
-sys.setrecursionlimit(10**7)
+# input은 왜 정의해야함?
+input = sys.stdin.readline
 
-#퀵정렬정의
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[0]
-    left = [x for x in arr[:1] if x <= pivot]
-    right = [x for x in arr[:1] if x > pivot]
+N = int(input())
+count = [0] * 10001
 
-    return quick_sort(left) + [pivot] + quick_sort(right)
+for _ in range(N) :
+    num = int(input())
+    count[num] += 1
 
-N = int(sys.stdin.readline())
-nums = [int(sys.stdin.readline()) for _ in range(N)]
+for i in range(1, 10001) :
+    if count[i] > 0 :
+        for _ in range(count[i]) :
+            print(i)
 
-sort_nums = quick_sort(nums)
-for num in sort_nums:
-    print(num)
